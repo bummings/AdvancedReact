31e1cbaca66db36d17e97eb2ac9e0a99decfa586
bummings <eabummings@gmail.com>
2018-12-15 17:11:25 -0500

    added styled components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+  background: red;
+  font-size: 8rem;
+`;
 
 class Page extends Component {
   render() {
@@ -8,6 +14,7 @@ class Page extends Component {
       <div>
         <Meta />
         <Header />
+        <MyButton>clik dat</MyButton>
         {this.props.children}
       </div>
     );
